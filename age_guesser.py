9c15ddfc4bb90c7a8685183fed90c074866c8252
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-08-24 13:11:51 -0600

    Completed Function

--- age_guesser.py
+++ age_guesser.py
@@ -1,6 +1,17 @@
 import random
 
-def greet(){
+def greet():
         print("Hello, I am Chat-not-gpt and I am going to guess your age")
-        input("What be ye name?")
+        name = input("What be yer name? ")
+        correctness = "\0"
+        while correctness != "y":
+            guess = random.randrange(15, 30)
+            print(guess)
+            correctness = input("Is this correct?")
+            if correctness != "y":
+                print("Rats!")
+        print(name + " is " + str(guess) + " years old.")
 
+greet()
+        
+            
