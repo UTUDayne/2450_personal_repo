0f0475e6a141cb792600ee663973c13ec238d068
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-08-24 13:13:16 -0600

    Added emergency exit to while loop

--- age_guesser.py
+++ age_guesser.py
@@ -4,10 +4,12 @@ def greet():
         print("Hello, I am Chat-not-gpt and I am going to guess your age")
         name = input("What be yer name? ")
         correctness = "\0"
-        while correctness != "y":
+        i = 0
+        while correctness != "y" or i > 50:
             guess = random.randrange(15, 30)
             print(guess)
             correctness = input("Is this correct?")
+            i += 1
             if correctness != "y":
                 print("Rats!")
         print(name + " is " + str(guess) + " years old.")
