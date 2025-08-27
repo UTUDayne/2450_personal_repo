a1c85c907ee4dde558fd86e87ffb7ad66146bf2d
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-08-27 10:38:48 -0600

    Added initial check and enhanced answer adaptibility

--- age_guesser.py
+++ age_guesser.py
@@ -5,14 +5,19 @@ def greet():
         name = input("What be yer name? ")
         correctness = "\0"
         i = 0
-        while correctness != "y" or i > 50:
+        initial_check = input("Are you between 15 and 30 years old?")
+        if initial_check[0].lower() != 'y':
+            print("Age out of range")
+            return
+        while correctness[0].lower() != "y" or i > 50:
             guess = random.randrange(15, 30)
             print(guess)
             correctness = input("Is this correct?")
             i += 1
-            if correctness != "y":
+            if correctness[0].lower() != "y":
                 print("Rats!")
         print(name + " is " + str(guess) + " years old.")
+
 
 greet()
         
